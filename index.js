913512f612b86ea6cf694bd71e67f9450ff4a148
Maru Di Primio <mariana.diprimio@gmail.com>
2022-09-28 19:31:06 +0200

    occupancyPercentage isOcuped ok

--- index.js
+++ index.js
@@ -1,29 +1,97 @@
+// Methods:
+// isOccupied(date) returns false if not occupied, returns true if occupied
+// occupancyPercentage(startDate, endDate) returns the percentage of days with occupancy within the range of dates provided (inclusive)
+
+    
 class Room {
-    constructor(){
+    constructor({ name, bookings, rate, discount }) {
+        this.name = name; // string
+        this.bookings = bookings; // array of booking objects
+        this.rate = rate; // int price in cents
+        this.discount = discount; // int percentage
+      }
+    
+    dateArray(startDate, endDate){
+        let start = new Date(startDate)
+        let end = new Date(endDate)
+        let dateArray = []
+    
+        while (start <= end) {
+            // console.log(start)
+            dateArray.push(new Date(start));
+            start = new Date(start.setDate(start.getDate() + 1)) 
+        }
+        return dateArray
+        }
 
-    }
     isOccupied(date){
-
+        // console.log(date, this.bookings);
+        for(let booking of this.bookings){
+            // console.log(booking);
+            // console.log(date >= booking.checkIn, date < booking.checkOut);
+            if(date >= booking.checkIn && date < booking.checkOut){
+                // console.log('RETURNING TRUE');
+                return true;
+            }
+        }
+        // console.log('RETURNING FALSE');
+        return false;
     }
+    
     occupancyPercentage(startDate, endDate){
-
+        const dates = this.dateArray(startDate, endDate);
+        console.log(dates)
+        console.log(dates.length)
+        let daysOccupied = [];
+        let daysOff = [];
+        for(let date of dates) {
+            console.log(date)
+            this.isOccupied(date) ? daysOccupied.push('+1 occupied') : daysOff.push('+1 off');
+            console.log(this.isOccupied(date))
+        }
+        let totalDaysOcuppied = daysOccupied.length;
+        let totalDaysOff = daysOff.length;
+        let totalDays = totalDaysOcuppied + totalDaysOff;
+        let result = totalDaysOcuppied * 100 / totalDays;
+        console.log(totalDays)
+        console.log(daysOff.length)
+        console.log(result)
+        return result
     }
 }
 
-class Booking {
-    constructor(){
+// Methods:
+// get fee() returns the fee, including discounts on room and booking
 
-    }
+class Booking {
+    constructor({ name, email, checkIn, checkOut, discount, room }) {
+        this.name = name; // string
+        this.email = email; // string
+        this.checkIn = checkIn; // date
+        this.checkOut = checkOut; // date
+        this.discount = discount; // int percentage
+        this.room = room; // a room object
+      }
 
     get fee(){
 
     }
 }
 
-function totalOcupancyPercentage(rooms, startDate, endDate){
+
+// También vamos a crear las siguientes funciones:
+// totalOccupancyPercentage(rooms, startDate, endDate) returns the total occupancy percentage across all rooms in the array
+// availableRooms(rooms, startDate, endDate) returns all rooms in the array that are not occupied for the entire duration
+// Nuestras clases y funciones estarán en el archivo index.js, y escribiremos otro archivo index.test.js
+// Escribiremos los tests primero, con jest, metiendo datos ficticios en las funciones y clases y calculando los resultados correctos a mano.
+// Escribimos un test por cada caso, no cada función / método. E.g.
+
+function totalOccupancyPercentage(rooms, startDate, endDate){
 
 }
 
 function availableRooms(rooms, startDate, endDate){
-    
+
 }
+
+module.exports = { Room, Booking, totalOccupancyPercentage, availableRooms };
---
+++ index.test.js
@@ -0,0 +1,71 @@
+
+const {
+    Room,
+    Booking,
+    totalOccupancyPercentage,
+    availableRooms,
+  } = require("./index");
+ 
+  //tests isOccupied()
+  test('room is occupied, search false, after bookings', () => {
+    const room = new Room({name: 'Pink',bookings: [], rate: 1000, discount: 30})
+    const booking1 = new Booking({name: 'Julia', email: 'julia@mymail.com', checkIn: '2022-10-01', checkOut: '2022-10-20', discount: 20, room: room})
+    const booking2 = new Booking({name: 'Alberto', email: 'albert@mymail.com', checkIn: '2022-11-13', checkOut: '2022-11-20', discount: 30, room: room})
+    room.bookings = [booking1, booking2];
+    expect(room.isOccupied('2022-12-01')).toBeFalsy()
+    
+  })
+
+  test('room is occupied, search false, before bookings', () => {
+    const room = new Room({name: 'Pink',bookings: [], rate: 1000, discount: 30})
+    const booking1 = new Booking({name: 'Julia', email: 'julia@mymail.com', checkIn: '2022-10-01', checkOut: '2022-10-20', discount: 20, room: room})
+    const booking2 = new Booking({name: 'Alberto', email: 'albert@mymail.com', checkIn: '2022-11-13', checkOut: '2022-11-20', discount: 30, room: room})
+    room.bookings = [booking1, booking2];
+    expect(room.isOccupied('2022-09-01')).toBeFalsy()
+  })
+
+  test('room is occupied, search false, just in checkOut', () => {
+    const room = new Room({name: 'Pink',bookings: [], rate: 1000, discount: 30})
+    const booking1 = new Booking({name: 'Julia', email: 'julia@mymail.com', checkIn: '2022-10-01', checkOut: '2022-10-20', discount: 20, room: room})
+    const booking2 = new Booking({name: 'Alberto', email: 'albert@mymail.com', checkIn: '2022-11-13', checkOut: '2022-11-20', discount: 30, room: room})
+    room.bookings = [booking1, booking2];
+    expect(room.isOccupied('2022-10-20')).toBeFalsy()
+  })
+
+  test('room is occupied, search true, in bookings', () => {
+    const room = new Room({name: 'Pink',bookings: [], rate: 1000, discount: 30})
+    const booking1 = new Booking({name: 'Julia', email: 'julia@mymail.com', checkIn: '2022-10-01', checkOut: '2022-10-20', discount: 20, room: room})
+    const booking2 = new Booking({name: 'Alberto', email: 'albert@mymail.com', checkIn: '2022-11-13', checkOut: '2022-11-20', discount: 30, room: room})
+    room.bookings = [booking1, booking2];
+    expect(room.isOccupied('2022-10-06')).toBe(true)
+
+  })
+  
+  test('room is occupied, search true, in bookings', () => {
+    const room = new Room({name: 'Pink',bookings: [], rate: 1000, discount: 30})
+    const booking1 = new Booking({name: 'Julia', email: 'julia@mymail.com', checkIn: '2022-10-01', checkOut: '2022-10-20', discount: 20, room: room})
+    const booking2 = new Booking({name: 'Alberto', email: 'albert@mymail.com', checkIn: '2022-11-13', checkOut: '2022-11-20', discount: 30, room: room})
+    room.bookings = [booking1, booking2];
+    expect(room.isOccupied('2022-11-15')).toBe(true)
+ 
+  })
+ 
+  test('room is occupied, search true, just in ckeckin', () => {
+    const room = new Room({name: 'Pink',bookings: [], rate: 1000, discount: 30})
+    const booking1 = new Booking({name: 'Julia', email: 'julia@mymail.com', checkIn: '2022-10-01', checkOut: '2022-10-20', discount: 20, room: room})
+    const booking2 = new Booking({name: 'Alberto', email: 'albert@mymail.com', checkIn: '2022-11-13', checkOut: '2022-11-20', discount: 30, room: room})
+    room.bookings = [booking1, booking2];
+    expect(room.isOccupied('2022-11-13')).toBe(true)
+ 
+  })
+
+  //tests occupancyPercentage()
+
+test('occupancyPercentage', () => {
+    
+    let room1 = new Room({name: 'SkyBlue',bookings: [], rate: 200, discount: 10})
+    let booking1 = new Booking({name: 'Antonella', email: 'antito@mymail.com', checkIn: '2022-01-10', checkOut: '2022-01-15', discount: 30, room: room1})
+    let booking2 = new Booking({name: 'Lorenzo', email: 'lolo@mymail.com', checkIn: '2022-05-01', checkOut: '2022-05-10', discount: 20, room: room1})
+    room1.bookings = [booking1, booking2];
+    expect(room1.occupancyPercentage('2022-01-02', '2022-01-12')).toBe(10)
+  })
